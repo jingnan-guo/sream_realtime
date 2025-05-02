8e0eb905671f2647c39680803b997d4f63e3e6ea
jingnan_guo <2849788678@qq.com>
2025-05-02 20:34:00 +0800

    实时部署

---
+++ stream_dev1/src/main/java/com/gjn/Main.java
@@ -0,0 +1,13 @@
+package com.gjn;
+
+/**
+ * @Package com.gjn.${NAME}
+ * @Author jingnan.guo
+ * @Date 2025/5/2 14:45
+ * @description: ${description}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
